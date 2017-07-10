1028abca815300891cea0e680af88b32572eb34c
Misha <misha.kuzn@gmail.com>
2017-07-10 18:11:50 -0500

    de

--- app/src/main/java/com/misha/myapplication/IngredientActivity.java
+++ app/src/main/java/com/misha/myapplication/IngredientActivity.java
@@ -1,11 +1,17 @@
 package com.misha.myapplication;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class IngredientActivity extends AppCompatActivity {
+
+    final Context context = this;
 
     @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,7 @@ public class IngredientActivity extends AppCompatActivity {
 
             public void onClick(View v) {
 
-                showMagic();
+                addIngredient();
 
             }
 
@@ -27,10 +33,26 @@ public class IngredientActivity extends AppCompatActivity {
 
     }
 
-    private void showMagic() {
+    private void addIngredient() {
 
-        View magic = findViewById(R.id.magic);
-        magic.setVisibility(View.VISIBLE);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+        final EditText et = new EditText(context);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(et);
+
+        // set dialog message
+        alertDialogBuilder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show it
+        alertDialog.show();
+
 
     }
 }
