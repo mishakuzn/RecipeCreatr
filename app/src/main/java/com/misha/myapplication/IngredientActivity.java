e8f95a39414562cd672c46f91d7e52f4736c2ac2
Misha <misha.kuzn@gmail.com>
2017-07-10 17:48:55 -0500

    de

--- app/src/main/java/com/misha/myapplication/IngredientActivity.java
+++ app/src/main/java/com/misha/myapplication/IngredientActivity.java
@@ -1,18 +1,16 @@
 package com.misha.myapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class IngredientActivity extends AppCompatActivity {
 
     @Override
    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_ingredient);
         Button button2 = (Button) findViewById(R.id.button2);
 
         button2.setOnClickListener(new View.OnClickListener() {
